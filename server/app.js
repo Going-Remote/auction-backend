b7fd612eca1cd1fa9bce1a62addcb434f407d840
otitoju <otitojuoluwapelumi@gmail.com>
2019-12-19 10:48:24 +0000

    chores setting up auction server and mvc

--- server/app.js
+++ server/app.js
@@ -1,12 +1,14 @@
 import express from 'express'
 const app = express()
 
+// port 
 const port = 4000
 
 app.get('/', (req, res) => {
     res.send(`<h1>Going Remote 2020 simulation server...`)
 })
 
+// server listen
 app.listen(port, () => {
     console.log(`Going Remote server started ${port}`)
 })
