399831c912835468efde9fe35422f8ee45f1d0a5
otitoju <otitojuoluwapelumi@gmail.com>
2019-12-23 21:55:48 +0000

    refactor merging branches

---
+++ server/test/app.test.js
@@ -0,0 +1,11 @@
+import chai from 'chai'
+import chaiHttp from 'chai-http'
+import app from '../app'
+
+// Configure chai
+chai.use(chaiHttp);
+chai.should();
+
+describe('Auction API tests', () => {
+    
+  })
